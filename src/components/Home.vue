a36dfd8e872c0e75823d8df84a850552e9633a6e
Hannasezen <andreeva0649@gmail.com>
2019-01-07 19:58:59 +0200

    adding tags to tagsList

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -68,11 +68,41 @@
               v-model="showSeriesMinutes"
             )
             p {{ showTime }}
-        .tag-list
-          .ui-tag__wrapper
+        .tag-list.tag-list--add
+          .ui-tag__wrapper(
+            @click="tagMenuShow = !tagMenuShow"
+          )
             .ui-tag
-              span.tag-title
-              span.button-close
+              span.tag-title Add new
+              span.button-close(
+                :class="{ active: !tagMenuShow }"
+              )
+        .tag-list.tag-list--menu(
+          v-if="tagMenuShow"
+        )
+          input.tag-add--input(
+            type="text"
+            placeholder="New tag"
+            v-model="tagTitle"
+            @keyup.enter="newTag"
+          )
+          .button.button-default(
+            @click="newTag"
+          ) Send
+        .tag-list
+          .ui-tag__wrapper(
+            v-for="tag in tags"
+            :key="tag.title"
+          )
+            .ui-tag(
+              @click="addTagUsed(tag)"
+              :class="{ active: tag.use }"
+            )
+              span.tag-title {{ tag.title }}
+              span.button-close(
+                :class="{ active: tag.use }"
+              )
+        p {{ tagsUsed }}
 </template>
 
 <script>
@@ -87,11 +117,28 @@ export default {
       filmMinutes: 30,
       showSeason: 1,
       showSeries: 11,
-      showSeriesMinutes: 40
+      showSeriesMinutes: 40,
+      tagTitle: '',
+      tagMenuShow: false,
+      tagsUsed: [],
+      tags: [
+        {title: 'comedy', use: false},
+        {title: 'western', use: false},
+        {title: 'adventure', use: false}
+      ]
     }
   },
   methods: {
-    newTask: function () {
+    newTag () {
+      if (this.tagTitle === '') {
+        return
+      }
+      this.tags.push({
+        title: this.tagTitle,
+        use: false
+      })
+    },
+    newTask () {
       if (this.taskTitle === '') {
         return
       }
@@ -107,13 +154,23 @@ export default {
         description: this.taskDescription,
         whatWatch: this.whatWatch,
         time,
+        tagsUsed: this.tagsUsed,
         completed: false,
         editing: false
       }
       console.log(task)
+      this.taskId++
       this.taskTitle = ''
       this.taskDescription = ''
-      this.taskId++
+      this.tagsUsed = []
+    },
+    addTagUsed (tag) {
+      tag.use = !tag.use
+      if (tag.use) {
+        this.tagsUsed.push(tag.title)
+      } else {
+        this.tagsUsed.splice(tag.title, 1)
+      }
     },
     getHoursAndMinutes (minutes) {
       let hours = Math.trunc(minutes / 60)
@@ -154,4 +211,33 @@ export default {
   .time-input
     max-width 80px
     margin-right 10px
+  .tag-list
+    margin-bottom 20px
+  .ui-tag__wrapper
+    margin-right 18px
+    margin-bottom 10px
+    &:last-child
+      margin 0
+  .ui-tag
+    &.active
+      background-color #f5f5f5
+    cursor pointer
+    .button-close
+      &.active
+        transform rotate(45deg)
+    &.used
+      background-color #444ce0
+      color #fff
+      .button-close
+        &:before,
+        &:after
+          background-color #fff
+  .tag-list--menu
+    display flex
+    justify-content space-between
+    align-items center
+  .tag-add--input
+    margin-bottom 0
+    margin-right 10px
+    height 42px
 </style>
